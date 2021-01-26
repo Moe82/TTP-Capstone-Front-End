7e626b75697c8fb453db6283f16b3184f7e74156
justinwu1 <justinwu820@gmail.com>
2021-01-26 11:09:42 -0500

    file upload form

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import RoutesContainer from '../components/AllPlayers/routes/RoutesContainer';
+import Axios from 'axios';
 
 class App extends Component {
   render() {
@@ -8,6 +9,11 @@ class App extends Component {
       <div className="app">
         <header className="app-header">
           <RoutesContainer />
+          <form style={{ textAlign: 'center' }} action = "localhost:8080/api/students/attendance/" method = "POST">
+            <label style={{ display: 'block' }}>Choose files to upload</label>
+            <input type="file" name="image" accept="image/pgn,image/jpeg" multiple />
+            <button type = "submit">Upload the images</button>
+          </form>
         </header>
       </div>
     );
