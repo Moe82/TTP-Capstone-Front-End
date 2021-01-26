d4460fcc79b641a9f6d2d9a7dedc21f50c0d4b04
justinwu1 <justinwu820@gmail.com>
2021-01-26 16:14:32 -0500

    imageToBase64 Done

--- src/components/AllPlayers/containers/UploadAttendanceContainer.js
+++ src/components/AllPlayers/containers/UploadAttendanceContainer.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import { UploadAttendanceView } from '../views'
-
 class UploadAttendanceContainer extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       file: null
     }
   }
-
-  handleChange = (event) => { this.setState({ file: event.target.files[0] }) }
+  _handleReaderLoaded = (readerEvt)=>{
+    let binaryStr = readerEvt.target.result;
+    this.setState({base64TextString:btoa(binaryStr)})
+  }
+  handleChange = (event) => { 
+    let file = event.target.files[0]
+    if(file){
+      const reader = new FileReader();
+      reader.onload = this._handleReaderLoaded.bind(this)
+      reader.readAsBinaryString(file);
+    }
+    this.setState({ file: event.target.files[0] })
+   }
 
   handleSubmit = (event) => {
     event.preventDefault()
-    const formData = new FormData(); 
+    const formData = new FormData();
     console.log(typeof this.state.file)
-    formData.append(this.state.file.name, this.state.file); 
+    formData.append(this.state.file.name, this.state.file);
     console.log("File:", this.state.file);
-    axios.post('http://localhost:8080/api/students/attendance', formData)
-    .then(res => {console.log(res)}).catch((error) => console.error(error));
+    console.log("Binary String:", this.state.base64TextString);
+    axios.post('http://localhost:8080/api/students/attendance', {
+      imgToBase64: this.state.base64TextString
+    })
+      .then(res => { console.log(res) }).catch((error) => console.error(error));
   }
-  
+
   render() {
     return (
       <div>
-        <UploadAttendanceView handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
+        <UploadAttendanceView handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
       </div>
-  )}
+    )
+  }
 }
 
 
