4e6f526128c7b0c11904c15eb352cecb280bd5c5
Mohamed Shafee <mshafee1993@gmail.com>
2021-01-30 11:07:35 -0500

    add conditional inside componentDidMount to fix #4

--- src/components/routes/RoutesContainer.jsx
+++ src/components/routes/RoutesContainer.jsx
@@ -9,7 +9,7 @@ import { NavbarContainer } from '../containers'
 
 class RoutesContainer extends Component {
   componentDidMount() {
-    (this.props.isLoggedIn == true ? console.log("Logged in") : this.props.loadInitialData(this.props.isLoggedIn));
+    (this.props.isLoggedIn == true ? console.log("Logged in") : this.props.);
   }
   render() {
     return (
