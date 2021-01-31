1b38a083dd9fd6ffaca38bd5d6144a43ffd9a98d
Mohamed Shafee <mshafee1993@gmail.com>
2021-01-31 14:13:40 -0500

    change input border color to fix #10

--- src/components/containers/Students/StudentList.js
+++ src/components/containers/Students/StudentList.js
@@ -3,51 +3,56 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createStudent, fetchStudents } from '../../../redux/player/StudentAction';
 class StudentList extends React.Component {
-    renderInput = ({ input, label, meta }) => {
-        return (
-            <div className="ui field">
-                <label>{label}</label>
-                <input style = {{width:'25%'}} {...input} />
-                <div>{this.renderError(meta)}</div>
-            </div>
-        )
-    }
-    onSubmit = (formValues) => {
-        this.props.createStudent(formValues, this.props.match.params.id);
-    }
-    renderList() {
-        { this.props.fetchStudents(this.props.match.params.id) };
-        return this.props.students.map(student => {
-            return (
-                <div className="item" key={student.id}>
-                    <div className="content" style={{ fontSize: '2rem' }}>
-                        {student.name}
-                    </div>
-                </div>
-            )
-        })
-    }
+    
+  renderInput = ({ input, label, meta }) => {
+    return (
+      <div className="ui field">
+        <label>{label}</label>
+        <input style = {{width:'25%', borderColor:'#AE81FF'}} {...input} />
+        <div>{this.renderError(meta)}</div>
+      </div>
+    )
+  }
+    
+  onSubmit = (formValues) => {
+    this.props.createStudent(formValues, this.props.match.params.id);
+  }
+    
+  renderList() {
+    { this.props.fetchStudents(this.props.match.params.id) };
+    return this.props.students.map(student => {
+      return (
+        <div className="item" key={student.id}>
+          <div className="content" style={{ fontSize: '2rem' }}>
+            {student.name}
+          </div>
+        </div>
+      )
+    })
+  }
+    
     renderError = ({ error, touched }) => {
-        if (touched && error) {
-            return (
-                <div className="ui error message">
-                    <div className="header">{error}</div>
-                </div>
-            )
-        }
-    }
-    render() {
+      if (touched && error) {
         return (
-            <div>
-                <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-                    <Field name="name" component={this.renderInput} label="Enter a student name or a multiple student name with commas (E.X: Adam L, Justin W)" />
-                    <button className="ui button primary">Add a student</button>
-                </form>
-                <h1>Students name</h1>
-                <div className="ui celled list">{this.renderList()}</div>
-            </div>
+          <div className="ui error message">
+            <div className="header">{error}</div>
+          </div>
         )
+      }
     }
+
+  render() {
+      return (
+          <div>
+              <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                  <Field name="name" component={this.renderInput} label="Enter a student name or a multiple student name with commas (E.X: Adam L, Justin W)" />
+                  <button style={{backgroundColor:'#AE81FF'}}className="ui button primary">Add a student</button>
+              </form>
+              <h1>Students name</h1>
+              <div className="ui celled list">{this.renderList()}</div>
+          </div>
+      )
+  }
 }
 
 const validate = (formValues) => {
